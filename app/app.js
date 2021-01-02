8015a899717f98948855e5bfe4117b7cd46fc35d
ryu0114h <ryu0114h@gmail.com>
2021-01-03 05:25:17 +0900

    CRUDなAPIの作成

--- app/app.js
+++ app/app.js
@@ -3,6 +3,10 @@ const app = express();
 const sqlite3 = require("sqlite3");
 const dbPath = "app/db/database.sqlite3";
 const path = require("path");
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname, "public")));
 
@@ -37,6 +41,75 @@ app.get("/api/v1/search", (req, res) => {
   db.all(`SELECT * FROM users WHERE name LIKE "%${keyword}%"`, (err, rows) => {
     res.json(rows);
   });
+
+  db.close();
+});
+
+const run = async (sql, db, res, message) => {
+  return new Promise((resolve, reject) => {
+    db.run(sql, (err) => {
+      if (err) {
+        res.status(500).send(err);
+        return reject();
+      } else {
+        res.json({ message: message });
+        return resolve();
+      }
+    });
+  });
+};
+
+// Create a user
+app.post("/api/v1/users", async (req, res) => {
+  const db = new sqlite3.Database(dbPath);
+
+  const name = req.body.name;
+  const profile = req.body.profile || "";
+  const dateOfBirth = req.body.date_of_birth || "";
+
+  await run(
+    `INSERT INTO users (name, profile, date_of_birth) VALUES ("${name}" , "${profile}" , "${dateOfBirth}")`,
+    db,
+    res,
+    "新規ユーザーを作成しました。"
+  );
+
+  db.close();
+});
+
+// Update user data
+app.put("/api/v1/users/:id", async (req, res) => {
+  const db = new sqlite3.Database(dbPath);
+  const id = req.params.id;
+
+  // 現在のユーザー情報を取得
+  db.get(`SELECT * FROM users WHERE id = ${id}`, async (err, row) => {
+    const name = req.body.name || row.name;
+    const profile = req.body.profile || row.profile;
+    const dateOfBirth = req.body.date_of_birth || row.date_of_birth;
+
+    await run(
+      `UPDATE users SET name="${name}", profile="${profile}", date_of_birth="${dateOfBirth}" WHERE id=${id}`,
+      db,
+      res,
+      "ユーザー情報を更新しました。"
+    );
+  });
+
+  db.close();
+});
+
+// Delete user data
+app.delete("/api/v1/users/:id", async (req, res) => {
+  const db = new sqlite3.Database(dbPath);
+  const id = req.params.id;
+
+  await run(
+    `DELETE FROM users WHERE id=${id}`,
+    db,
+    res,
+    "ユーザー情報を削除しました。"
+  );
 
   db.close();
 });
