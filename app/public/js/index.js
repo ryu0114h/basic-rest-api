9f0e9cd3ebe64aba7080d003c9a2764382fb57c8
ryu0114h <ryu0114h@gmail.com>
2021-01-03 06:05:58 +0900

    ユーザー作成フォームとAPIを繋げる

--- app/public/js/index.js
+++ app/public/js/index.js
@@ -1,8 +1,25 @@
 const indexModule = (() => {
-  // 検索ボタンをクリックした際のイベントを設定
-  document
-    .getElementById("search-btn")
-    .addEventListener("click", searchModule.searchUsers);
-  // UserモジュールのfetchAllUsersメソッドを呼び出す
-  return usersModule.fetchAllUsers();
+  const path = window.location.pathname;
+
+  switch (path) {
+    case "/":
+      // 検索ボタンをクリックした際のイベントを設定
+      document
+        .getElementById("search-btn")
+        .addEventListener("click", searchModule.searchUsers);
+      // UserモジュールのfetchAllUsersメソッドを呼び出す
+      return usersModule.fetchAllUsers();
+
+    case "/create.html":
+      document
+        .getElementById("save-btn")
+        .addEventListener("click", usersModule.createUser);
+      document
+        .getElementById("cancel-btn")
+        .addEventListener("click", () => (window.location.href = "/"));
+      break;
+
+    default:
+      break;
+  }
 })();
